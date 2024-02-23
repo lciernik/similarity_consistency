8629862a8bed5ded8b118afcac28ac843813b184
lciernik <laure.ciernik@gmail.com>
2024-02-23 09:49:38 +0100

    test run

--- benchmark/myscripts/model_configs.sh
+++ benchmark/myscripts/model_configs.sh
@@ -23,3 +23,20 @@ echo "Model names: $model_names"
 echo "Pretrained values: $pretrained_values"
 echo "Source values: $source_values"
 echo "Model parameters values: $model_parameters_values"
+
+
+#clip_benchmark eval --dataset=cifar10 \
+#                    --task=linear_probe \
+#                    --pretrained="$pretrained_values" \
+#                    --model="$model_names" \
+#                    --output=result.json \
+#                    --batch_size=64 \
+#                    --fewshot_lr 0.1 \
+#                    --fewshot_epochs 20 \
+#                    --batch_size 512 \
+#                    --train_split train \
+#                    --test_split test \
+#                    --model_source="$source_values" \
+#                    --model_parameters="$model_parameters_values"
+
+#                    --fewshot_k 5 \
