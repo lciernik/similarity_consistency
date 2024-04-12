df088e975d9acd11452f29a8f2ed3adee58a9f68
lciernik <ciernik@tu-berlin.de>
2024-04-12 11:59:08 +0200

    starting writing python run scripts

---
+++ benchmark/scripts/feature_extraction.py
@@ -0,0 +1,67 @@
+import os
+import json
+from slurm import run_job
+
+MODEL_CONFIG = "./models_config.json"
+DATASETS = "./webdatasets.txt"
+
+BASE_PROJECT_PATH = "/home/space/diverse_priors"
+DATASETS_ROOT = os.path.join(BASE_PROJECT_PATH, 'datasets', 'wds', 'wds_{dataset_cleaned}')
+
+FEATURES_ROOT = os.path.join(BASE_PROJECT_PATH, 'features')
+
+OUTPUT_ROOT = os.path.join(BASE_PROJECT_PATH, 'results', 'single_models', '{fewshot_k}', '{dataset}', '{model}',
+                           'fewshot_lr_{fewshot_lr}', 'fewshot_epochs_{fewshot_epochs}', 'seed_{seed}')
+g
+
+def load_models():
+    with open(MODEL_CONFIG, "r") as file:
+        models = json.load(file)
+    return models
+
+
+def get_hyperparms():
+    hyper_params = dict(
+        fewshot_lrs=['0.1', '0.01'],
+        fewshot_ks=['-1', '5', '10', '100'],
+        fewshot_epochs=['10', '20', '30'],
+        seeds=[str(num) for num in range(10)],
+    )
+    num_jobs = len(list(product(*hyper_params.values())))
+    return hyper_params, num_jobs
+
+
+if __name__ == "__main__":
+    models = load_models()
+    n_models = len(models)
+
+    hyper_params, num_jobs = get_hyperparms()
+
+    for key, model_config in models.items():
+        job_cmd = f"""export XLA_PYTHON_CLIENT_PREALLOCATE=false && \
+        export XLA_PYTHON_CLIENT_ALLOCATOR=platform && \
+        clip_benchmark eval --dataset {DATASETS} \
+                            --dataset_root {DATASETS_ROOT} \
+                            --feature_root {FEATURES_ROOT} \
+                            --output {OUTPUT_ROOT} \
+                            --task=linear_probe \
+                            --model {model_config['model_name']} \
+                            --model_source {model_config['source']} \
+                            --model_parameters {model_config['model_parameters']} \
+                            --module_name {model_config['module_name']} \
+                            --batch_size=64 \
+                            --fewshot_k {' '.join(hyper_params['fewshot_ks'])} \
+                            --fewshot_lr {' '.join(hyper_params['fewshot_lrs'])} \
+                            --fewshot_epochs {' '.join(hyper_params['fewshot_epochs'])} \
+                            --train_split train \
+                            --test_split test \
+                            --seed {' '.join(seeds)} 
+        """
+
+        run_job(
+            job_name=f"feat_extr_{key}",
+            job_cmd=job_cmd,
+            partition='gpu-2d',
+            log_dir='./logs',
+            num_jobs_in_array=1
+        )
---
+++ benchmark/scripts/slurm.py
@@ -0,0 +1,70 @@
+from slurmpy import Slurm
+
+
+def run_job(
+        job_name,
+        job_cmd,
+        num_cpus=16,
+        partition='gpu-2d',
+        apptainer=False,
+        slurm_args=None,
+        log_dir='./logs',
+        num_jobs_in_array=1,
+):
+    if apptainer:
+        raise NotImplementedError("No apptainer container available for now.")
+        # squash_data_dir = '/tmp'
+        # copy_cmd = ["echo 'Copy data to /tmp'",
+        #             "cp -r /home/space/datasets/histopathology/anomaly-detection/squash/* /tmp",
+        #             "ls -l /tmp"]
+        # copy_cmd = '&&'.join(copy_cmd)
+        #
+        # submit_cmd = f"""
+        # {copy_cmd} && apptainer run --nv \
+        #      -B /home/space/datasets/histopathology/anomaly-detection/colon/20x/splits:/squash/colon/20x/splits \
+        #      -B /home/space/datasets/histopathology/anomaly-detection/stomach/20x/splits:/squash/stomach/20x/splits \
+        #      -B /home/space/datasets/histopathology/anomaly-detection/splits:/squash/splits \
+        #      -B {squash_data_dir}/colon/20x/normal/colon_HE_l01_vS007_normal_20x_80ua_images_sn_sl.sqfs:/squash/colon/20x/normal/colon_HE_l01_vS007_normal_20x_80ua_images_sn_sl:image-src=/ \
+        #      -B {squash_data_dir}/colon/20x/anomaly/colon_HE_l01_vS009_anomaly_hc_20x_80ua_images_sn_sl.sqfs:/squash/colon/20x/anomaly/colon_HE_l01_vS009_anomaly_hc_20x_80ua_images_sn_sl:image-src=/ \
+        #      -B {squash_data_dir}/stomach/20x/normal/stomach_normal_HE_l01_vS005_20x_80ua_images_sn_sl.sqfs:/squash/stomach/20x/normal/stomach_normal_HE_l01_vS005_20x_80ua_images_sn_sl:image-src=/ \
+        #      -B {squash_data_dir}/stomach/20x/anomaly/stomach_HE_l01_vS005_anomaly_hc_20x_80ua_images_sn_sl.sqfs:/squash/stomach/20x/anomaly/stomach_HE_l01_vS005_anomaly_hc_20x_80ua_images_sn_sl:image-src=/ \
+        #      -B {squash_data_dir}/oe/outlier_exposure_HE_l01_vS001_20x_images_sn_sl.sqfs:/squash/oe/outlier_exposure_HE_l01_vS001_20x_images_sn_sl:image-src=/ \
+        #      -B {squash_data_dir}/oe-v2/20x/outlier_exposure_HE_l01_vS002_20x_10ua_images_sn_sl.sqfs:/squash/oe-v2/20x/outlier_exposure_HE_l01_vS002_20x_10ua_images_sn_sl:image-src=/ \
+        #      -B {squash_data_dir}/oe-small-intestine/colon_mixed_HE_l01_vS007_20x_80ua_images_sn_sl.sqfs:/squash/oe-small-intestine/colon_mixed_HE_l01_vS007_20x_80ua_images_sn_sl:image-src=/ \
+        #      -B {squash_data_dir}/oe-small-intestine/stomach_mixed_HE_l01_vS005_20x_80ua_images_sn_sl.sqfs:/squash/oe-small-intestine/stomach_mixed_HE_l01_vS005_20x_80ua_images_sn_sl:image-src=/ \
+        #      container.sif {job_cmd}
+        # """
+    else:
+        submit_cmd = job_cmd
+
+    slurm_options = {
+        "partition": partition,
+        "cpus-per-task": num_cpus,
+        "exclude": "head[026-033]",
+        "nodes": 1,
+        "chdir": "./",
+        "output": f"{log_dir}/run_%A/%a.out",
+        "error": f"{log_dir}/run_%A/%a.out",
+        "array": f"0-{num_jobs_in_array-1}" if num_jobs_in_array > 1 else "0"
+    }
+
+    if slurm_args is not None:
+        for key, val in slurm_args.items():
+            slurm_options[key] = val
+
+    device, time = partition.split('-')
+
+    if device == "gpu":
+        slurm_options["gres"] = "gpu:1"
+
+    time_mapping = {
+        "test": "00-00:15:00",
+        "9m": "00-00:09:00",
+        "2h": "00-02:00:00",
+        "5h": "00-05:00:00",
+        "2d": "02-00:00:00",
+        "7d": "07-00:00:00"
+    }
+    slurm_options["time"] = time_mapping[time]
+    s = Slurm(job_name, slurm_options)
+    s.run(submit_cmd)
