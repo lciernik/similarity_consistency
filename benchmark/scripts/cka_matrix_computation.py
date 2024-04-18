aca9cc54aedbe2cb9fe2c6ee2ac2c5155cf75d8c
lciernik <laure.ciernik@gmail.com>
2024-04-19 00:01:14 +0200

    minor change default value

--- benchmark/scripts/cka_matrix_computation.py
+++ benchmark/scripts/cka_matrix_computation.py
@@ -4,10 +4,13 @@ from slurm import run_job
 from helper import load_models, prepare_for_combined_usage
 
 MODELS_CONFIG = "./models_config.json"
-DATASETS = "./imagenet_subset.txt"
 
 BASE_PROJECT_PATH = "/home/space/diverse_priors"
+
+# DATASETS = "./webdatasets.txt"
 # DATASETS_ROOT = os.path.join(BASE_PROJECT_PATH, 'datasets', 'wds', 'wds_{dataset_cleaned}')
+
+DATASETS = "./imagenet_subset.txt"
 DATASETS_ROOT = os.path.join(BASE_PROJECT_PATH, 'datasets')
 
 FEATURES_ROOT = os.path.join(BASE_PROJECT_PATH, 'features')
@@ -16,15 +19,21 @@ private_out_root = "/home/lciernik/projects/divers-priors/results_local/cka"
 OUTPUT_ROOT = os.path.join(private_out_root, 'imagenet_subset_10k')
 
 
+def _count_nr_datasets():
+    with open(DATASETS, 'r') as f:
+        return len(f.readlines())
+
+
 if __name__ == "__main__":
     # Retrieve the configuration of all models we intend to evaluate.
     models, n_models = load_models(MODELS_CONFIG)
     print(f"Run CKA distance matrix experiment with {n_models} models.")
-
+    # Prepare the models for combined usage.
     model_names, sources, model_parameters, module_names = prepare_for_combined_usage(models)
 
-    # Evaluate each model on all datasets and all hyperparameter configurations.
-
+    # Nr of jobs in the array is equal to the number of datasets.
+    njobs = _count_nr_datasets()
+    print(f"Nr.jobs: {njobs}")
     job_cmd = f"""export XLA_PYTHON_CLIENT_PREALLOCATE=false && \
                         export XLA_PYTHON_CLIENT_ALLOCATOR=platform && \
                         clip_benchmark eval --dataset {DATASETS} \
@@ -41,7 +50,7 @@ if __name__ == "__main__":
     run_job(
         job_name=f"CKA",
         job_cmd=job_cmd,
-        partition='gpu-5h',
+        partition='gpu-2d',
         log_dir='./logs',
-        num_jobs_in_array=1
+        num_jobs_in_array=njobs,
     )
