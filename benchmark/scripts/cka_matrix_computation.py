4f9702397947744e0d77ccbf06602f65c654a377
lciernik <laure.ciernik@gmail.com>
2024-04-18 17:26:54 +0200

    typo

--- benchmark/scripts/cka_matrix_computation.py
+++ benchmark/scripts/cka_matrix_computation.py
@@ -1,7 +1,7 @@
 import os
 import json
 from slurm import run_job
-from helper import load_models
+from helper import load_models, prepare_for_combined_usage
 
 MODELS_CONFIG = "./models_config_small.json"
 DATASETS = "wds/voc2007"
@@ -14,32 +14,33 @@ DATASETS_ROOT = os.path.join(BASE_PROJECT_PATH, 'datasets', 'wds', 'wds_{dataset
 FEATURES_ROOT = os.path.join(BASE_PROJECT_PATH, 'features')
 
 private_out_root = "/home/lciernik/projects/divers-priors/results_local/cka"
-OUTPUT_ROOT = os.path.join(private_out_root, )
+OUTPUT_ROOT = private_out_root
 
 if __name__ == "__main__":
     # Retrieve the configuration of all models we intend to evaluate.
     models, n_models = load_models(MODELS_CONFIG)
 
-    # Evaluate each model on all datasets and all hyperparameter configurations.
-    for key, model_config in models.items():
-        job_cmd = f"""export XLA_PYTHON_CLIENT_PREALLOCATE=false && \
-        export XLA_PYTHON_CLIENT_ALLOCATOR=platform && \
-        clip_benchmark eval --dataset {DATASETS} \
-                            --dataset_root {DATASETS_ROOT} \
-                            --feature_root {FEATURES_ROOT} \
-                            --output {OUTPUT_ROOT} \
-                            --task=model_similarity \
-                            --model {model_config['model_name']} \
-                            --model_source {model_config['source']} \
-                            --model_parameters '{json.dumps(model_config['model_parameters'])}' \
-                            --module_name {model_config['module_name']} \
-                            --train_split train
-        """
+    model_names, sources, model_parameters, module_names = prepare_for_combined_usage(models)
 
-        run_job(
-            job_name=f"feat_extr_{key}",
-            job_cmd=job_cmd,
-            partition='gpu-5h',
-            log_dir='./logs',
-            num_jobs_in_array=1
-        )
+    # Evaluate each model on all datasets and all hyperparameter configurations.
+
+    job_cmd = f"""export XLA_PYTHON_CLIENT_PREALLOCATE=false && \
+                        export XLA_PYTHON_CLIENT_ALLOCATOR=platform && \
+                        clip_benchmark eval --dataset {DATASETS} \
+                                            --dataset_root {DATASETS_ROOT} \
+                                            --feature_root {FEATURES_ROOT} \
+                                            --output {OUTPUT_ROOT} \
+                                            --task=model_similarity \
+                                            --model {' '.join(model_names)} \
+                                            --model_source {' '.join(sources)} \
+                                            --model_parameters {' '.join([f"'{json.dumps(x)}'" for x in model_parameters])} \
+                                            --module_name {' '.join(module_names)} \
+                                            --train_split train
+                    """
+    run_job(
+        job_name=f"fCKA",
+        job_cmd=job_cmd,
+        partition='gpu-5h',
+        log_dir='./logs',
+        num_jobs_in_array=1
+    )
