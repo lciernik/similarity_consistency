e7f7a632425453321e01bc58092ca578f9833974
lciernik <laure.ciernik@gmail.com>
2024-04-19 12:04:12 +0200

    reformat

--- benchmark/clip_benchmark/metrics/distance_matrix.py
+++ benchmark/clip_benchmark/metrics/distance_matrix.py
@@ -24,8 +24,10 @@ def _load_features(feature_root, model_id, split='train'):
 
 def _check_models(feature_root, model_ids, split):
     prev_model_ids = model_ids
+
     model_ids = sorted(
         [mid for mid in model_ids if os.path.exists(os.path.join(feature_root, mid, f'features_{split}.pt'))])
+
     if len(set(prev_model_ids)) != len(set(model_ids)):
         print(f"Features do not exist for the following models: {set(prev_model_ids) - set(model_ids)}")
         print(f"Removing the above models from the list of models for distance computation.")
