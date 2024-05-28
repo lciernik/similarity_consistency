fdf7073a37579384b52e788be9794789fe86ac77
lciernik <laure.ciernik@gmail.com>
2024-05-28 17:45:10 +0200

    Changed cluster_models.py drastically + new run_clustering.py file that computes the clustering for different metrics and num_clusters.

--- benchmark/clip_benchmark/cluster_models.py
+++ benchmark/clip_benchmark/cluster_models.py
@@ -1,49 +1,76 @@
-from scripts.helper import load_models, prepare_for_combined_usage
-import pandas as pd
-import numpy as np
-import torch
-from sklearn.cluster import SpectralClustering, AffinityPropagation
 import os
 
-corr_method = "spearman"
-rsa_mat = torch.load(
-    f"/home/space/diverse_priors/model_similarities/rsa_correlation_{corr_method}/imagenet_subset_10k/rsa_distance_matrix.pt")
-
-dataset = 'imagenet-subset-10k'
-models, n_models = load_models('./scripts/models_config.json')
-num_clusters = 4
-
-model_names, sources, model_parameters, module_names = prepare_for_combined_usage(models)
-models = zip(model_names, sources, model_parameters, module_names)
+import numpy as np
+import pandas as pd
+import torch
+from sklearn.cluster import SpectralClustering
 
 
-def _get_model_id(model, model_parameters):
-    if not model_parameters:
-        return model
-    model_slug = model
-    model_suffix = model_parameters.get("variant", "")
-    if model_suffix:
-        model_slug = f"{model_slug}_{model_suffix}"
-    model_suffix = model_parameters.get("dataset", "")
-    if model_suffix:
-        model_slug = f"{model_slug}_{model_suffix}"
-    return model_slug
+def check_paths(args):
+    sim_mat_path = os.path.join(args.sim_mat_root, args.dataset, args.method_key, 'similarity_matrix.pt')
+    model_ids_path = os.path.join(args.sim_mat_root, args.dataset, args.method_key, 'model_ids.txt')
+    if not os.path.exists(sim_mat_path):
+        raise FileNotFoundError(f"Similarity matrix not found at {sim_mat_path}")
+    if not os.path.exists(model_ids_path):
+        raise FileNotFoundError(f"Model ids not found at {model_ids_path}")
+    return sim_mat_path, model_ids_path
 
 
-# Get model ids
-model_ids = [_get_model_id(model[0], model[2]) for model in models]
-model_ids = [(model_id + '-' + dataset).replace('/', '_') for model_id in model_ids]
-model_ids = sorted(model_ids)
-model_ids = [mid.replace(f"-{dataset}", "") for mid in model_ids]
+def load_similarity_matrix(sim_mat_path, model_ids_path):
+    sim_mat = torch.load(sim_mat_path)
+    with open(model_ids_path, 'r') as f:
+        model_ids = f.read().splitlines()
+    return pd.DataFrame(sim_mat, index=model_ids, columns=model_ids)
 
-# compute affinity mat
-df = pd.DataFrame(rsa_mat.numpy(), index=model_ids, columns=model_ids)
-affinity_mat = df.copy().abs()
-np.fill_diagonal(affinity_mat.values, 1)
 
-clustering = SpectralClustering(n_clusters=num_clusters,
-                                affinity='precomputed',
-                                assign_labels='discretize',
-                                random_state=0)
+def process_similarity_matrix(sim_mat):
+    sim_mat = sim_mat.abs()
+    if not np.all(np.diag(sim_mat.values) == 1):
+        np.fill_diagonal(sim_mat.values, 1)
+    return sim_mat
 
-labels = clustering.fit_predict(affinity_mat.values, y=None)
+
+def main(args):
+    sim_mat_path, model_ids_path = check_paths(args)
+    sim_mat = load_similarity_matrix(sim_mat_path, model_ids_path)
+    sim_mat = process_similarity_matrix(sim_mat)
+
+    clustering = SpectralClustering(n_clusters=args.num_clusters,
+                                    affinity='precomputed',
+                                    assign_labels=args.assign_labels,
+                                    random_state=args.seed)
+
+    labels = clustering.fit_predict(sim_mat.values, y=None)
+    labels = pd.DataFrame(labels, index=sim_mat.index, columns=['cluster'])
+
+    output_path = os.path.join(args.output_root, args.dataset, args.method_key)
+    if not os.path.exists(output_path):
+        os.makedirs(output_path)
+    labels.to_csv(os.path.join(output_path, 'cluster_labels.csv'))
+
+
+
+if __name__ == '__main__':
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--num_clusters', type=int, help="Number of clusters to create.")
+    parser.add_argument('--method_key', type=str,
+                        choices=['cka_kernel_linear_unbiased', 'cka_kernel_rbf_unbiased_sigma_0.6',
+                                 'rsa_method_correlation_corr_method_pearson', 'cka_kernel_rbf_unbiased_sigma_0.2',
+                                 'cka_kernel_rbf_unbiased_sigma_0.8', 'rsa_method_correlation_corr_method_spearman',
+                                 'cka_kernel_rbf_unbiased_sigma_0.4'],
+                        default='rsa_correlation_spearman')
+
+    parser.add_argument('--dataset', type=str, default='imagenet-subset-10k',
+                        help="Dataset to use for clustering.")
+    parser.add_argument('--assign_labels', type=str, default='kmeans',
+                        choices=['kmeans', 'discretize', 'cluster_qr'],
+                        help="Method used to assign labels during SpectralClustering.")
+    parser.add_argument('--seed', type=int, default=0, help="Random seed for reproducibility.")
+    parser.add_argument('--sim_mat_root', type=str, default="/home/space/diverse_priors/model_similarities")
+    parser.add_argument('--output_root', type=str, default="/home/space/diverse_priors/clustering")
+
+    args = parser.parse_args()
+
+    main(**vars(args))
---
+++ benchmark/scripts/run_clustering.py
@@ -0,0 +1,37 @@
+from slurm import run_job
+
+SIM_MAT_ROOT = "/home/space/diverse_priors/model_similarities"
+OUTPUT_ROOT = "/home/space/diverse_priors/clustering"
+
+METHOD_KEYS = [
+    'cka_kernel_rbf_unbiased_sigma_0.2',
+    'cka_kernel_rbf_unbiased_sigma_0.4'
+    'cka_kernel_rbf_unbiased_sigma_0.6',
+    'cka_kernel_rbf_unbiased_sigma_0.8',
+    'cka_kernel_linear_unbiased',
+    'rsa_method_correlation_corr_method_pearson',
+    'rsa_method_correlation_corr_method_spearman',
+]
+
+NUM_CLUSTERS = range(2, 12)
+
+if __name__ == "__main__":
+    for num_clusters in NUM_CLUSTERS:
+        for method_key in METHOD_KEYS:
+            job_cmd = f"""
+            python clip_benchmark/cluster_models.py  \
+                --num_clusters {num_clusters} \
+                --method_key {method_key} \
+                --dataset imagenet-subset-10k \
+                --assign_labels kmeans \
+                --seed 0 \
+                --sim_mat_root {SIM_MAT_ROOT} \
+                --output_root {OUTPUT_ROOT}
+            """
+            run_job(
+                job_name=f"clustering",
+                job_cmd=job_cmd,
+                partition='cpu-9m',
+                log_dir='./logs',
+                num_jobs_in_array=1
+            )
