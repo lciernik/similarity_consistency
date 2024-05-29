921ba30453766cc0b158aab03d51c26ecda21a2d
Jonas Dippel <jonas.dippel@gmx.de>
2024-05-29 23:49:26 +0200

    implemented retrieve performance based on database results

--- benchmark/clip_benchmark/analysis/utils.py
+++ benchmark/clip_benchmark/analysis/utils.py
@@ -1,13 +1,14 @@
 import sqlite3
 import pandas as pd
-from functools import partial
 
 RESULTS_PATH = ''
 
 
-def retrieve_performance(model_id: str, dataset_id: str):
-    """conn = sqlite3.connect(RESULTS_PATH)
+def retrieve_performance(model_id: str, dataset_id: str, metric_column: str = 'test_lp_acc1'):
+    path = f"/home/space/diverse_priors/results/linear_probe/single_model/{dataset_id}/{model_id}/results.db"
+    conn = sqlite3.connect(path)
     df = pd.read_sql('SELECT * FROM "results"', conn)
-    conn.close()"""
-    # TODO
-    return 1
+    conn.close()
+    subset = df[df.dataset == f'"{dataset_id}"'][metric_column].values.max()
+    performance = subset.max()
+    return performance
