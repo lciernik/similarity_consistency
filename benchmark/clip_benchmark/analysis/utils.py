af6d38b3402d02f7af8de3111ef53b4504494324
Jonas Dippel <jonas.dippel@gmx.de>
2024-06-04 23:58:01 +0200

    added results_root option to retrieve_performance

--- benchmark/clip_benchmark/analysis/utils.py
+++ benchmark/clip_benchmark/analysis/utils.py
@@ -1,11 +1,11 @@
 import sqlite3
 import pandas as pd
+import os
 
-RESULTS_PATH = ''
 
-
-def retrieve_performance(model_id: str, dataset_id: str, metric_column: str = 'test_lp_acc1'):
-    path = f"/home/space/diverse_priors/results/linear_probe/single_model/{dataset_id}/{model_id}/results.db"
+def retrieve_performance(model_id: str, dataset_id: str, metric_column: str = 'test_lp_acc1',
+                         results_root='/home/space/diverse_priors/results/linear_probe/single_model'):
+    path = os.path.join(results_root, dataset_id, model_id, "results.db")
     conn = sqlite3.connect(path)
     df = pd.read_sql('SELECT * FROM "results"', conn)
     conn.close()
