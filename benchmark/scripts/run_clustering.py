e84d8da8f68e3ad2e26e7b1d32feb0939a1e5022
lciernik <ciernik@tu-berlin.de>
2024-07-16 11:25:34 +0200

    added and modified some utility functions

--- benchmark/scripts/run_clustering.py
+++ benchmark/scripts/run_clustering.py
@@ -1,5 +1,7 @@
 from slurm import run_job
+from helper import load_models
 
+MODELS_CONFIG = "./filtered_models_config.json"
 SIM_MAT_ROOT = "/home/space/diverse_priors/model_similarities"
 OUTPUT_ROOT = "/home/space/diverse_priors/clustering"
 
@@ -20,6 +22,13 @@ LBL_ASSIGN_METHODS = ['kmeans', 'discretize', 'cluster_qr']
 DATASET = "imagenet-subset-10k"
 
 if __name__ == "__main__":
+    models, n_models = load_models(MODELS_CONFIG)
+    if 'SegmentAnything_vit_b' in models.keys():
+        models.pop('SegmentAnything_vit_b')
+    model_keys = ' '.join(models.keys())
+
+    array = f"0"
+
     for num_clusters in NUM_CLUSTERS:
         for method_key in METHOD_KEYS:
             for lbl_assign_method in LBL_ASSIGN_METHODS:
@@ -31,12 +40,13 @@ if __name__ == "__main__":
                     --assign_labels {lbl_assign_method} \
                     --seed 0 \
                     --sim_mat_root {SIM_MAT_ROOT} \
-                    --output_root {OUTPUT_ROOT} 
+                    --output_root {OUTPUT_ROOT} \
+                    --model_key {model_keys}
                 """
                 run_job(
                     job_name=f"clustering",
                     job_cmd=job_cmd,
                     partition='cpu-9m',
                     log_dir=f'{OUTPUT_ROOT}/logs',
-                    num_jobs_in_array=1
+                    array=array
                 )
