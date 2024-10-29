b5137f17b1318d343bfe650827c6533312a1f770
lciernik <ciernik@tu-berlin.de>
2024-10-29 20:01:27 +0100

    additional file defining project structure

---
+++ benchmark/scripts/project_location.py
@@ -0,0 +1,19 @@
+import os
+
+## DEFINE BASEPATHS
+BASE_PATH_PROJECT = '/home/space/diverse_priors'
+
+### DEFINE SUBFOLDERS
+
+DATASETS_ROOT = os.path.join(BASE_PROJECT_PATH, 'datasets')
+
+SUBSET_ROOT = os.path.join(DATASETS_ROOT, 'subsets')
+
+FEATURES_ROOT = os.path.join(BASE_PROJECT_PATH, 'features')
+
+MODELS_ROOT = os.path.join(BASE_PROJECT_PATH, 'models')
+
+MODEL_SIM_ROOT = os.path.join(BASE_PROJECT_PATH, 'model_similarities')
+
+RESULTS_ROOT = os.path.join(BASE_PROJECT_PATH, 'results')
+
